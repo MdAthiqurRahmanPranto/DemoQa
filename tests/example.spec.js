7a9f24d8af06e654b62daefbfb1aac35da8fb38d
Pranto <athiqurrahmanpranto@gmail.com>
2025-05-28 11:32:34 +0600

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,20 @@
+// tests/clickWidget.spec.js
+
+const { test, expect } = require('@playwright/test');
+
+test('Click on Widgets card on demoqa homepage', async ({ page }) => {
+  // Go to the homepage
+  await page.goto('https://demoqa.com/');
+  await page.locator('div:nth-child(2) > div > .card-up').click();
+  await page.locator('span').filter({ hasText: 'Forms' }).locator('div').first().click();
+  await page.getByRole('listitem').click();
+  await page.getByRole('textbox', { name: 'First Name' }).click();
+  await page.getByRole('textbox', { name: 'First Name' }).fill('asddas');
+  await page.getByRole('textbox', { name: 'Last Name' }).click();
+  await page.getByRole('textbox', { name: 'Last Name' }).fill('d');
+  await page.getByText('Male', { exact: true }).click();
+  await page.getByRole('button', { name: 'Submit' }).click();
+
+  // Optional: Assert you're on the right page
+  //await expect(page).toHaveURL(/.*widgets/);
+});
